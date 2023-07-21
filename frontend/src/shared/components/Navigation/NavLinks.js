7eb870c8241c7f52befa2954a673bb2966114dca
rit-kr <rit.kr.7@gmail.com>
2023-07-21 16:24:47 +0530

    context added

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,21 +1,30 @@
 import './NavLinks.css';
-import React from 'react';
+import React, { useContext } from 'react';
 import{ NavLink } from 'react-router-dom'
+import { AuthContext } from '../../context/auth-context';
 const NavLinks = props => {
+    const auth = useContext(AuthContext)
+
     return(
         <ul className='nav-links'>
             <li>
                 <NavLink to="/" exact>All Users</NavLink>
             </li>
-            <li>
+            {auth.isLoggedIn && (
+                <li>
                 <NavLink to="/ul/places">My Places</NavLink>
             </li>
+            )}
+             {auth.isLoggedIn && (
             <li>
                 <NavLink to="/places/new">Add place</NavLink>
             </li>
+            )}
+            {!auth.isLoggedIn && (
             <li>
                 <NavLink to="/auth">Authentication</NavLink>
             </li>
+            )}
         </ul>
     )
 
--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -1,15 +1,19 @@
 import Button from '../../shared/components/FormElements/Button';
 import Input from '../../shared/components/FormElements/Input';
 import Card from '../../shared/components/UIElements/Card';
+import { AuthContext } from '../../shared/context/auth-context';
 import { useForm } from '../../shared/hooks/form-hook';
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/Validators';
 import './Auth.css'
-import React from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 
 
 
 const Auth = () =>{
-    const [formState, inputHandler] = useForm({
+    const auth = useContext(AuthContext);
+
+    const [isLoginMode, setIsLoginMode] = useState(true);
+    const [formState, inputHandler, setFormData] = useForm({
         email:{
             value:'',
             isValid:false
@@ -20,16 +24,48 @@ const Auth = () =>{
         }
     })
 
+    const switchModeHandler = () => {
+         if(!isLoginMode){
+            setFormData({
+                    ...formState.inputs,
+                    name: undefined
+                },
+                formState.inputs.email.isValid && formState.inputs.password.isValid
+            )
+         } else{
+            setFormData({
+                ...formState.inputs,
+                    name:{
+                        value:'',
+                        isValid:false
+                    }
+                },false)
+         }
+        setIsLoginMode(prevMode => !prevMode);
+    };
+
     const authSubmitHandler = event => {
         event.preventDefault();
         console.log((formState.inputs));
+        auth.login();
     };
-    
+
     return(
         <Card className="authentication" >
             <h2>Login Required</h2>
             <hr />
             <form onSubmit={authSubmitHandler}>
+                {!isLoginMode && 
+                    <Input 
+                    element="input"
+                    id="name"
+                    type="name"
+                    label="Your Name"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a valid name"
+                    onInput={inputHandler}
+                />
+                }
                 <Input 
                     element="input"
                     id="email"
@@ -48,8 +84,9 @@ const Auth = () =>{
                     errorText="Please enter a valid password, at least 8 characters"
                     onInput={inputHandler}
                 />
-                <Button type="submit" disabled={!formState.isValid}>Login</Button>
+                <Button type="submit" disabled={!formState.isValid}>{isLoginMode ? "LOGIN" : "SIGNUP"}</Button>
             </form>
+            <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}</Button>
         </Card>
     )
 }
